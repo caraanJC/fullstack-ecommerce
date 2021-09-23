3a61a3c1109e0b63b28adeb2aa6630d564e84b70
caraanJC <caraanjohncarlo@gmail.com>
2021-09-23 19:27:42 +0800

    feat: remove redux devtools

--- src/state/store.js
+++ src/state/store.js
@@ -1,12 +1,7 @@
 import { applyMiddleware, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
-const store = createStore(
-  reducers,
-  {},
-  composeWithDevTools(applyMiddleware(thunk))
-);
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 export { store };
